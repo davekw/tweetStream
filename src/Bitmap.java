e50218d302b667e0b451062a7f558d0d799915ba
Dave Kwiatkowski <dave1@vt.edu>
2016-02-05 23:05:12 -0500

    Update Bitmap.java

--- src/Bitmap.java
+++ src/Bitmap.java
@@ -63,6 +63,18 @@ public class Bitmap {
         }
     }
 
+    public void draw3(String string, int x, int y, int col) {
+        for (int i = 0; i < string.length(); i++) {
+            int ch = chars.indexOf(string.charAt(i));
+            if (ch < 0) continue;
+
+            int xx = ch % 42;
+            int yy = ch / 42;
+            draw(Art.font3, x + i * 6 * 8, y, xx * 6 * 8, yy * 8 * 8, 5 * 8,
+                    8 * 8, col);
+        }
+    }
+
     public void draw4(String string, int x, int y, int col) {
         for (int i = 0; i < string.length(); i++) {
 
@@ -79,67 +91,6 @@ public class Bitmap {
             int yy = 2 ;
             draw(Art.font2, x + i * 6 * 4, y, xx * 6 * 4, yy * 8 * 4,
                     5 * 4, 8 * 4, col);
-        }
-    }
-
-    public void draw3(String string, int x, int y, int col) {
-        for (int i = 0; i < string.length(); i++) {
-            int ch = chars.indexOf(string.charAt(i));
-            if (ch < 0) continue;
-
-            int xx = ch % 42;
-            int yy = ch / 42;
-            draw(Art.font3, x + i * 6 * 8, y, xx * 6 * 8, yy * 8 * 8, 5 * 8,
-                    8 * 8, col);
-        }
-    }
-
-    public void draw(String string, int x, int y, int col) {
-        for (int i = 0; i < string.length(); i++) {
-            int ch = chars.indexOf(string.charAt(i));
-            if (ch < 0) continue;
-
-            int xx = ch % 42;
-            int yy = ch / 42;
-            draw(Art.font, x + i * 6, y, xx * 6, yy * 8, 5, 8, col);
-        }
-    }
-
-    //draws from right to left
-    public void flipDraw(Bitmap bitmap, int xOffs, int yOffs, int xo, int yo,
-                         int w, int h, int col) {
-        for (int y = 0; y < h; y++) {
-            int yPix = y + yOffs;
-            if (yPix < 0 || yPix >= height) continue;
-
-            for (int x = 0; x < w; x++) {
-                int xPix = xOffs - x + 16;
-                if (xPix < 0 || xPix >= width) continue;
-
-                int src = bitmap.pixels[(x + xo) + (y + yo) * bitmap.width];
-                if (src >= 0) {
-                    pixels[xPix + yPix * width] = src & col;
-                }
-            }
-        }
-    }
-
-    public void scaleDraw(Bitmap bitmap, int scale, int xOffs, int yOffs,
-                          int xo, int yo, int w, int h, int col) {
-        for (int y = 0; y < h * scale; y++) {
-            int yPix = y + yOffs;
-            if (yPix < 0 || yPix >= height) continue;
-
-            for (int x = 0; x < w * scale; x++) {
-                int xPix = x + xOffs;
-                if (xPix < 0 || xPix >= width) continue;
-
-                int src = bitmap.pixels[(x / scale + xo)
-                        + (y / scale + yo) * bitmap.width];
-                if (src >= 0) {
-                    pixels[xPix + yPix * width] = src * col;
-                }
-            }
         }
     }
 
