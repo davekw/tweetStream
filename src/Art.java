ca5104607cbb5bee04d51cff8fe75b05d67f7597
Dave Kwiatkowski <dave1@vt.edu>
2016-02-05 23:31:52 -0500

    Update Art.java

--- src/Art.java
+++ src/Art.java
@@ -4,16 +4,17 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class Art {
-    public static Bitmap font = loadBitmap("/font.png");
     public static Bitmap font2 = loadBitmap("/font2.png");
     public static Bitmap font3 = loadBitmap("/font3.png");
+    public static Bitmap at = loadBitmap("/at.png");
     public static Bitmap bird = loadPure("/bird.png");
     public static Bitmap feed = loadPure("/feed2.png");
-    public static Bitmap at = loadBitmap("/at.png");
-
 
     /**
-     * words
+     * Creates bitmap that need post coloring
+     *
+     * @param fileName The name of the file to be mapped
+     * @return a bitmap object to be on the screen
      */
     public static Bitmap loadBitmap(String fileName) {
         try {
@@ -36,6 +37,12 @@ public class Art {
         }
     }
 
+    /**
+     * Creates bitmap that uses original coloring
+     *
+     * @param fileName The name of the file to be mapped
+     * @return a bitmap object to be placed on the screen
+     */
     public static Bitmap loadPure(String fileName) {
         try {
             BufferedImage img = ImageIO.read(Art.class.getResource(fileName));
@@ -57,6 +64,12 @@ public class Art {
         }
     }
 
+    /**
+     * Creates bitmap from url with no post coloring
+     *
+     * @param urlString The name of the url image to be mapped
+     * @return a bitmap object to be placed on the screen
+     */
     public static Bitmap loadUrl(String urlString) {
         try {
             URL url = new URL(urlString);
